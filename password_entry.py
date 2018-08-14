ef959c0adc6d4ddf0c6d27b1f0b3ab984d679ad9
Kate <kate.tsakissiris@my.jcu.edu.au>
2018-08-14 15:02:23 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,13 @@
 """
 Kate Tsakissiris
 """
+
+password = input("Enter a password: ")
+if len(password) < 2:
+    print("Invalid - password is too small")
+    password = input("Enter a password: ")
+if len(password) > 10:
+    print("Invalid - password is too large")
+    password = input("Enter a password: ")
+print('*' * len(password))
+print()
